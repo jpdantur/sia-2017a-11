f7d5a70523e47ba57b79ab30d5430a4ada119c9d
Juan Pablo Dantur <wally@Juans-MacBook-Pro.local>
2017-05-05 11:00:02 -0300

    fix ErrorGrapher

--- matlab/src/ErrorGrapher.m
+++ matlab/src/ErrorGrapher.m
@@ -1,19 +1,20 @@
 classdef ErrorGrapher < handle
 		
-	properties (Access = protected)
-		errors = {}
+	properties
+		errors = [];
 	end
 
 
 	methods
-		function output = addError(outputs,targets)
-			errors = [errors targets-outputs];
+		function output = addError(this, outputs,targets)
+			this.errors = [this.errors; targets-outputs];
 		end
 
-		function output = graph(epochs)
+		function output = graph(this, epochs)
 			x = 1:epochs;
-			y = 0.5*sum(errors{x}.^2);
-			plot(x,y);
+			y = 0.5*sum(this.errors(x,:).^2,2);
+			scatter(x,y)
+			output = y;
 		end
 	end
 end
