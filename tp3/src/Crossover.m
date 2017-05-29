95db318afb82cf168d83e16262b7a951ddba62aa
Ariel <ndebrouvier@itba.edu.ar>
2017-05-29 18:01:35 -0300

    cruza de un punto para poder testear mutación

--- tp3/src/Crossover.m
+++ tp3/src/Crossover.m
@@ -12,6 +12,9 @@
 		properties (Access = protected)
 
 			% Propiedades...
+
+			config;
+
 		end
 
 		methods (Access = public)
@@ -19,18 +22,55 @@
 			% Constructor:
 			function this = Crossover(config)
 
-				% TODO: Completar...
+				this.config = config;
+
 			end
 
 			% Selecciona los siguientes individuos:
 			function population = recombine(this, population)
+
+				selected = population;
+
+				population = {};
 
 				% IMPORTANTE: este método no debe modificar los cromosomas en
 				%	'population', sino copiar el contenido y generar nuevos
 				%	cromosomas.
 
 				% TODO: Completar...
-				population = {};
+
+				for i = 1:length(selected)-1
+
+					if rand < this.config.crossoverProbability
+
+						parent1 = selected{i};
+
+						length(parent1.genes)
+
+						parent2 = selected{i+1};
+
+						descendant = this.pointCross(parent1,parent2,randi(1,5)); %% TODO Parametrizar o random?
+
+						population{end+1} = descendant;
+
+					end
+
+				end
+
+			end
+
+			function descendant = pointCross(this,parent1,parent2,point)
+
+				[parent1.genes(1:point) parent2.genes(point+1:6)]
+
+				descendant = Chromosome([parent1.genes(1:point) parent2.genes(point+1:6)]); %%TODO Reemplazar 6 por end
+
+			end
+
+			function descendant = twopointCross(this,parent1,parent2,points)
+
+				%descendant = Chromosome([parent1.genes(1:points(1)) parent2.genes(1,points(1):points(2)) [parent1.genes(points(2):end)])
+
 			end
 		end
 	end
