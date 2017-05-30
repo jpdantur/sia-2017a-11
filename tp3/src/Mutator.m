e19d386a28608d2cbb8df919ddd8d85fdb12c03e
Juan Pablo Dantur <wally@Juans-MacBook-Pro.local>
2017-05-29 22:32:26 -0300

    Little fix

--- tp3/src/Mutator.m
+++ tp3/src/Mutator.m
@@ -23,6 +23,7 @@
 
 			% Selecciona los siguientes individuos:
 			function population = mutate(this, population)
+				global data;
 
 				% IMPORTANTE: este m??todo puede modificar libremente los
 				%	cromosomas en 'population', ya que no son los originales
@@ -30,7 +31,7 @@
 
 
 				for n = 1:length(population)
-					population{n}.genes(1:5) = mod(((population{n}.genes(1:5)-1) + binornd(1,this.config.mutationProbability,1,5).*randi([1 9],1,5)),10)+1;
+					population{n}.genes(1:5) = mod(((population{n}.genes(1:5)-1) + binornd(1,this.config.mutationProbability,1,5).*randi([1 (size(data{1},1)-1)],1,5)),size(data{1},1))+1;
 					if (rand<this.config.mutationProbability)
 						population{n}.genes(6) = 1.3 + rand * (2.0-1.3);
 					end
